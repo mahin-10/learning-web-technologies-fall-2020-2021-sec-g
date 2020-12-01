c1d943990dfe1dad7a0feb3bdd4847651bc26f22
Sadman Mahin <70022697+mahin-10@users.noreply.github.com>
2020-12-01 09:23:52 +0600

    last commit

---
+++ FINAL_LAB_TASK_1.2_PHP/JOB_portal.php
@@ -0,0 +1,31 @@
+<?php
+
+if(isset($_POST["inssert"]))
+
+echo
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title></title>
+    </head>
+
+        <form action="JOB_portal.php" method="post">
+        
+            <input type="text" name="name" placeholder="Employer name"><br>
+            <input type="text" name="name" placeholder="company name"><br>
+            <input type="number" name="contact num" placeholder="contact num"><br>
+            <input type="text" name="user name" placeholder="username"><br>
+            <input type="number" name="password" placeholder="password"><br>
+            <div>
+                <input type="submit" name="insert" value="update">
+                <input type="submit" name="insert" value="Delete">
+                <input type="submit" name="insert" value="Search">
+            </div>
+
+
+        </form>
+
+</html>
